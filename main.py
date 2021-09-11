cc090fcd9772042e6469feeff89754f66519a394
zaj-e <carbonelmarcelo@gmail.com>
2021-09-11 00:04:07 -0500

    checkpoint

--- main.py
+++ main.py
@@ -1,10 +1,82 @@
-from fastapi import FastAPI
+import numpy as np
+from io import BytesIO
+from PIL import Image
+from fastapi import (FastAPI, File, UploadFile)
+from starlette.responses import RedirectResponse
+from tensorflow.python.keras.preprocessing import image as image_preprocessing
+import tensorflow as tf
+from tensorflow.python.keras.applications.inception_resnet_v2 import InceptionResNetV2
+from tensorflow.python.keras.applications.inception_resnet_v2 import preprocess_input, decode_predictions
+from googletrans import Translator, constants
+
+model = None
 
 app = FastAPI()
+translator = Translator()
 
-#domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
+
+def load_model():
+    inception_model = InceptionResNetV2(weights='imagenet')
+    if inception_model is None:
+        print("Model NOT loaded")
+    else:
+        print("Model  loaded")
+    return inception_model
 
 
-@app.get("/")
-def home():
-    return {"message":"Hello TutLinks.com"}
+def predict(img: Image.Image):
+    global model
+    if model is None:
+        model = load_model()
+    print("imagen ")
+    print(img)
+    img = np.asarray(img.resize((299, 299)))[..., :3]
+    x = image_preprocessing.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+
+    predictions = model.predict(x)
+    print('Prediction:', decode_predictions(predictions, top=1)[0][0])
+    res = decode_predictions(predictions, top=1)[0][0]
+
+    response = []
+
+    resp = {"class": res[1], "confidence": f"{res[2] * 100:0.2f} %"}
+
+    response.append(resp)
+
+    return response
+
+
+def read_image_file(file) -> Image.Image:
+    image = Image.open(BytesIO(file))
+    return image
+
+
+@app.get("/", include_in_schema=False)
+async def index():
+    return RedirectResponse(url="/docs")
+
+
+@app.post("/predict/image")
+async def predict_api(file: UploadFile = File(...)):
+    extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png")
+    print(file.filename)
+    print(extension)
+    if not extension:
+        return "Image must be jpg or png format!"
+
+    image = read_image_file(await file.read())
+
+    prediction = predict(image)
+
+    prediction_text = prediction[0]['class']
+    prediction_text = prediction_text.replace("_", " ")
+    print(prediction_text)
+
+    print("X")
+    translation = translator.translate(prediction_text, "es")
+    translation = translation.text
+    print(translation)
+
+    return translation
